512841cbef5f129100e779a035dcea943d1d4ac8
mikhacito <mikhaelmiro@gmail.com>
2020-12-17 16:15:37 +0100

    removed event handler (for now)

--- commands/clientv.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
-    message.channel.send(`${message.author.toString()}, Brocket's current version is: 1.0`)
-};
-
-exports.help = {
-    name: "clientv",
-    description: "client version."
-}
